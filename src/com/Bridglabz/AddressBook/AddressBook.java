9876436c607bd8cf87403aa726678156e7dba568
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-11-30 22:35:28 +0530

    Added: UC3 Update Contact details

--- src/com/Bridglabz/AddressBook/AddressBook.java
+++ src/com/Bridglabz/AddressBook/AddressBook.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class AddressBook {
-    HashSet set=new HashSet();
+    HashSet<Contacts> set=new HashSet<>();
     Scanner sc=new Scanner(System.in);
     public void addContact(){
         System.out.println("Add details: ");
@@ -29,6 +29,54 @@ public class AddressBook {
         set.add(contacts);
         System.out.println("Contact details added successfully...." + set);
     }
+
+    // Update Contact details method.....
+    public void updateContacts() {
+        boolean found = false;
+        System.out.println("Enter your first name: ");
+        Scanner scanner = new Scanner(System.in);
+        String firstName = scanner.nextLine();
+        for (Contacts contacts11:set) {
+            if (contacts11.getFirstName().equals(contacts11.firstName)) {
+                System.out.println("Enter the first name: ");
+                String firstName1 = sc.nextLine();
+                System.out.println("Enter the last name: ");
+                String lastName = sc.nextLine();
+                System.out.println("Enter the address: ");
+                String address = sc.nextLine();
+                System.out.println("Enter the city: ");
+                String city = sc.nextLine();
+                System.out.println("Enter the state: ");
+                String state = sc.nextLine();
+                System.out.println("Enter the zip code: ");
+                int zip = sc.nextInt();
+                System.out.println("Enter the Phone number: ");
+                long phone1 = sc.nextLong();
+                System.out.println("Enter the email: ");
+                String email = sc.nextLine();
+                contacts11.setFirstName(firstName1);
+                contacts11.setLastName(lastName);
+                contacts11.setAddress(address);
+                contacts11.setCity(city);
+                contacts11.setState(state);
+                contacts11.setZip(zip);
+                contacts11.setPhone(phone1);
+                contacts11.setEmail(email);
+                System.out.println(contacts11);
+                System.out.println("Contact details updated.");
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Contact first name  is not present....");
+        } else {
+            System.out.println("Contact details updated successfully....");
+        }
+    }
+
+
+
+
     static boolean ordering = true;
     public static void main(String[] args) {
         AddressBook addressBook=new AddressBook();
@@ -52,8 +100,9 @@ public class AddressBook {
                     addressBook.addContact();
                     break;
                 // Update Contacts.....
-                case 3:
+                case 2:
                     System.out.println("Update Contacts: ");
+                    addressBook.updateContacts();
                     break;
                 // Delete Contacts....
                 case 4:
